07e6b02e0b1f9a2c03aef89d376617af4181f811
yuppieeeee <150029081+yuppieeeee@users.noreply.github.com>
2024-12-03 16:57:19 +0100

    Kommentar

--- World.java
+++ World.java
@@ -4,7 +4,7 @@ public static void main (String[] args){
 
 System.out.println("Hello World");
 System.out.println("Das Ergebnis lautet: " + addition(1,2));
-
+// Testkommentar
 }
 
 public static int addition(int x, int y){
