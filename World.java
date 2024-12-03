e2414ebc6ac66057bf73c0ded8d7cf613aa35459
Max de Pater <depatermax@gmail.com>
2024-12-03 17:06:43 +0100

    Test

--- World.java
+++ World.java
@@ -4,11 +4,16 @@ public static void main (String[] args){
 
 System.out.println("Hello World");
 System.out.println("Das Ergebnis lautet: " + addition(1,2));
+System.out.println("Die multilikation lautet: " + multilikation(3,10));
 // Testkommentar
 }
 
 public static int addition(int x, int y){
     return x+y;
+}
+
+public static int multiplikation(int x, int y) {
+    return x * y;
 }
 
 }
