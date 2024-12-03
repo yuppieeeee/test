675420a81249e29412bd179713046d72ff6cf90b
yupieeeee <ac@betterdoc.de>
2024-12-03 16:55:29 +0100

    Addition

--- World.java
+++ World.java
@@ -3,6 +3,12 @@ public class World{
 public static void main (String[] args){
 
 System.out.println("Hello World");
+System.out.println("Das Ergebnis lautet: " + addition(1,2));
 
 }
+
+public static int addition(int x, int y){
+    return x+y;
+}
+
 }
